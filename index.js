2e6dbd75f065eff884df859e122a5ef2d14b781e
FedeSca001 <familadore@gmail.com>
2024-04-21 12:32:59 +0200

    add Yuan

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const selectorDolarBnaCompra = "#market-scrll-2 > tbody > tr:nth-child(1) > td.b
 const selectorDolarBnaVenta = "#market-scrll-2 > tbody > tr:nth-child(1) > td.sell > a > div > div.sell-value";    
 const selectorDolarTurisata = "#market-scrll-2 > tbody > tr:nth-child(3) > td.sell > a > div > div.sell-value";
 const selectorEuroDolar = ".text-5xl\\/9.font-bold.text-\\[\\#232526\\]"; // Selector simplificado para la cotización euro-dólar
-const selectorEuroYen = 'div[data-test="instrument-price-last"]';
+const selectorEuroYuan = 'div[data-test="instrument-price-last"]';
 
 
 let cotizacion = {
@@ -24,7 +24,7 @@ let cotizacion = {
     precioDolarTurista: "",
   },
   euroDolar: "",
-  euroYen: "",
+  euroYuan: "",
   fecha: {
     dia: "",
     hora: ""
@@ -56,7 +56,7 @@ const updateData = async () => {
   try {
     const {data} = await axios.get('https://es.investing.com/currencies/eur-cny');
     const $ = cheerio.load(data);
-    cotizacion.euroYen = $(selectorEuroYen).text();
+    cotizacion.euroYuan = $(selectorEuroYuan).text();
   } catch (error) {
     console.log(error);
   }

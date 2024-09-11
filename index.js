412b55fedceca1241b16196b744eed824a407c01
FedeSca001 <familadore@gmail.com>
2024-09-11 17:30:08 +0200

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from "express";
+import cors from 'cors';  // Importar cors
 import monedasRouter from './src/monedas.js';
 import cryptoRouter from './src/crypto.js';
-import motogpRouter from './src/motogp.js'
-import mgpMotorSport from './src/motogpMotorSport.js'
-import clasificacionMotogp from './src/clasificacionMotogp.js'
+import motogpRouter from './src/motogp.js';
+import mgpMotorSport from './src/motogpMotorSport.js';
+import clasificacionMotogp from './src/clasificacionMotogp.js';
+
 const PORT = process.env.PORT || 5050;
 const app = express();
+
+// Configurar CORS
+app.use(cors());
 
 // Ruta principal
 app.get("/", (req, res) => {

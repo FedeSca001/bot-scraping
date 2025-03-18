25819c59b5c8815c2ccb5e5f0a8db21421e4ba9a
FedeSca001 <familadore@gmail.com>
2025-03-18 18:01:35 +0100

    server-local

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Configurar CORS
 app.use(cors({
-  origin: ['https://show-me-the-data.netlify.app', 'http://localhost:5173'],
+  origin: 'https://show-me-the-data.netlify.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   exposedHeaders: ['Custom-Header'],
